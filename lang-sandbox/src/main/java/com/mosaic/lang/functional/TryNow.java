22c0af7856844feed81654a594822b01932acdf4
Chris <ck@zeebox.com>
2013-10-23 06:51:25 +0100

    documented TryNow, and added another util method

--- lang-sandbox/src/main/java/com/mosaic/lang/functional/TryNow.java
+++ lang-sandbox/src/main/java/com/mosaic/lang/functional/TryNow.java
@@ -11,7 +11,10 @@ public class TryNow {
 
     public static final TryNbl NULL = successfulNbl(Nullable.NULL);
 
-    
+    /**
+     * Wraps the specified operation with a try/catch block and returns
+     * the result as an instance of Try.
+     */
     public static <T> Try<T> tryNow( Function0<T> op ) {
         try {
             return successful( op.invoke() );
@@ -20,14 +23,25 @@ public class TryNow {
         }
     }
 
+    /**
+     * Factory method for Try.  Captures the specified value as a happy path result.
+     */
     public static <T> Try<T> successful( T v ) {
         return Future.successful(v);
     }
 
+    /**
+     * Factory method for Try.  Captures information as to why the operation
+     * failed.
+     */
     public static <T> Try<T> failed( Failure f ) {
         return Future.failed(f);
     }
 
+    /**
+     * Wraps the specified operation with a try/catch block and returns
+     * the result as an instance of TryNbl.
+     */
     public static <T> TryNbl<T> tryNowNbl( Function0<Nullable<T>> op ) {
         try {
             return successfulNbl(op.invoke());
@@ -36,10 +50,24 @@ public class TryNow {
         }
     }
 
+    /**
+     * Factory method for TryNbl.  Captures the specified value as a success case.
+     */
+    public static <T> TryNbl<T> successfulNbl( T v ) {
+        return FutureNbl.successful( Nullable.createNullable(v) );
+    }
+
+    /**
+     * Factory method for TryNbl.  Captures the specified value as a success case.
+     */
     public static <T> TryNbl<T> successfulNbl( Nullable<T> v ) {
         return FutureNbl.successful(v);
     }
 
+    /**
+     * Factory method for TryNbl.  Captures information as to why the operation
+     * failed.
+     */
     public static <T> TryNbl<T> failedNbl( Failure f ) {
         return FutureNbl.failed(f);
     }
