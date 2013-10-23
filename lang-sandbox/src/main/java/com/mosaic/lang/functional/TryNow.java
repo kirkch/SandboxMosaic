e173bc434e2cbd54d165b17fe48a6c42d0b0c143
Chris <ck@zeebox.com>
2013-10-23 06:43:46 +0100

    added more util methods to TryNow

--- lang-sandbox/src/main/java/com/mosaic/lang/functional/TryNow.java
+++ lang-sandbox/src/main/java/com/mosaic/lang/functional/TryNow.java
@@ -2,6 +2,7 @@ package com.mosaic.lang.functional;
 
 import com.mosaic.lang.Failure;
 import com.mosaic.utils.concurrent.Future;
+import com.mosaic.utils.concurrent.FutureNbl;
 
 /**
  * Factory class for jobs that execute immediately.
@@ -10,10 +11,34 @@ public class TryNow {
 
     public static <T> Try<T> tryNow( Function0<T> op ) {
         try {
-            return Future.successful( op.invoke() );
+            return successful( op.invoke() );
         } catch ( Exception e ) {
-            return Future.failed( new Failure(e) );
+            return failed( new Failure(e) );
         }
+    }
+
+    public static <T> Try<T> successful( T v ) {
+        return Future.successful(v);
+    }
+
+    public static <T> Try<T> failed( Failure f ) {
+        return Future.failed(f);
+    }
+
+    public static <T> TryNbl<T> tryNowNbl( Function0<Nullable<T>> op ) {
+        try {
+            return successfulNbl(op.invoke());
+        } catch ( Exception e ) {
+            return failedNbl( new Failure(e) );
+        }
+    }
+
+    public static <T> TryNbl<T> successfulNbl( Nullable<T> v ) {
+        return FutureNbl.successful(v);
+    }
+
+    public static <T> TryNbl<T> failedNbl( Failure f ) {
+        return FutureNbl.failed(f);
     }
 
 }
