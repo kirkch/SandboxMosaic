f550fa5598768e0659f8a311bb1b88d8e5f24c65
Chris Kirk <ck@softwaremosaic.co.uk>
2014-04-20 11:11:03 +0100

    added iterator to IntRange

--- lang-sandbox/src/main/java/com/mosaic/lang/math/IntRange.java
+++ lang-sandbox/src/main/java/com/mosaic/lang/math/IntRange.java
@@ -1,11 +1,13 @@
 package com.mosaic.lang.math;
 
 import java.io.Serializable;
+import java.util.Iterator;
+
 
 /**
  *
  */
-public class IntRange implements Serializable {
+public class IntRange implements Serializable, Iterable<Integer> {
     private static final long serialVersionUID = 1290455330488L;
 
     private int min;
@@ -19,5 +21,26 @@ public class IntRange implements Serializable {
 
     public boolean contains( int v ) {
         return min <= v && v < maxExc;
+    }
+
+    public Iterator<Integer> iterator() {
+        return new Iterator<Integer>() {
+            private int next = min;
+
+            public boolean hasNext() {
+                return next < maxExc;
+            }
+
+            public Integer next() {
+                int v = next;
+
+                next += 1;
+
+                return v;
+            }
+
+            public void remove() {
+            }
+        };
     }
 }
