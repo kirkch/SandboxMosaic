896eb5c69db04f10cd272a395ec376bbee57be07
Chris Kirk <ck@softwaremosaic.co.uk>
2014-06-17 05:49:37 +0100

    added a generic parameter to Columns

--- columnstore-sandbox/src/main/java/com/mosaic/columnstore/Columns.java
+++ columnstore-sandbox/src/main/java/com/mosaic/columnstore/Columns.java
@@ -6,26 +6,96 @@ import com.mosaic.columnstore.columns.IntColumnArray;
 import com.mosaic.columnstore.columns.LongColumnArray;
 import com.mosaic.columnstore.columns.ObjectColumnArray;
 import com.mosaic.io.streams.CharacterStream;
-import com.mosaic.lang.functional.Int2BooleanFunction;
 import com.mosaic.lang.functional.Long2BooleanFunction;
 import com.mosaic.lang.text.UTF8;
 
-import java.util.Arrays;
 import java.util.Iterator;
 
 
 /**
  *
  */
-public class Columns implements Iterable<Column> {
+public class Columns<T extends Column> implements Iterable<T> {
+
+    public static BooleanColumn newBooleanColumn( String columnName, String description, boolean...values ) {
+        BooleanColumn col = new BooleanColumnArray( columnName, description, values.length );
+
+        for ( int i=0; i<values.length; i++ ) {
+            col.set(i, values[i]);
+        }
+
+        return col;
+    }
+
+    public static IntColumn newIntColumn( String columnName, String description, int...values ) {
+        IntColumn col = new IntColumnArray( columnName, description, values.length );
+
+        for ( int i=0; i<values.length; i++ ) {
+            col.set(i, values[i]);
+        }
+
+        return col;
+    }
+
+    public static LongColumn newLongColumn( String columnName, String description, long...values ) {
+        LongColumn col = new LongColumnArray( columnName, description, values.length );
+
+        for ( int i=0; i<values.length; i++ ) {
+            col.set(i, values[i]);
+        }
+
+        return col;
+    }
+
+    public static FloatColumn newFloatColumn( String columnName, String description, float...values ) {
+        FloatColumn col = new FloatColumnArray( columnName, description, values.length );
+
+        for ( int i=0; i<values.length; i++ ) {
+            col.set(i, values[i]);
+        }
+
+        return col;
+    }
+
+    public static <T> ObjectColumn<T> newObjectColumn( String columnName, String description, T...values ) {
+        ObjectColumn<T> col = new ObjectColumnArray<>( columnName, description, values.length );
+
+        for ( int i=0; i<values.length; i++ ) {
+            col.set(i, values[i]);
+        }
+
+        return col;
+    }
+
+
+
+
+
+
 
     private static final UTF8 SEPARATOR = new UTF8( ", " );
 
 
-    private Column[] columns;
+    private T[] columns;
 
-    public Columns( Column...columns ) {
+    public Columns( T...columns ) {
         this.columns = columns;
+    }
+
+    public T getColumn( int index ) {
+        return columns[index];
+    }
+
+    public int indexOf( String targetColumnName ) {
+        for ( int i=0; i<columns.length; i++ ) {
+            T col = columns[i];
+
+            if ( col.getColumnName().equals(targetColumnName) ) {
+                return i;
+            }
+        }
+
+        return -1;
     }
 
     public void writeAsCSVTo( CharacterStream out ) {
@@ -91,58 +161,22 @@ public class Columns implements Iterable<Column> {
         return count;
     }
 
-    public static BooleanColumn newBooleanColumn( String columnName, String description, boolean...values ) {
-        BooleanColumn col = new BooleanColumnArray( columnName, description, values.length );
+    public Iterator<T> iterator() {
+        return new Iterator<T>() {
+            private int index = 0;
 
-        for ( int i=0; i<values.length; i++ ) {
-            col.set(i, values[i]);
-        }
+            public boolean hasNext() {
+                return index < columns.length;
+            }
 
-        return col;
+            public T next() {
+                return columns[index++];
+            }
+
+            public void remove() {
+                throw new UnsupportedOperationException();
+            }
+        };
     }
 
-    public static IntColumn newIntColumn( String columnName, String description, int...values ) {
-        IntColumn col = new IntColumnArray( columnName, description, values.length );
-
-        for ( int i=0; i<values.length; i++ ) {
-            col.set(i, values[i]);
-        }
-
-        return col;
-    }
-
-    public static LongColumn newLongColumn( String columnName, String description, long...values ) {
-        LongColumn col = new LongColumnArray( columnName, description, values.length );
-
-        for ( int i=0; i<values.length; i++ ) {
-            col.set(i, values[i]);
-        }
-
-        return col;
-    }
-
-    public static FloatColumn newFloatColumn( String columnName, String description, float...values ) {
-        FloatColumn col = new FloatColumnArray( columnName, description, values.length );
-
-        for ( int i=0; i<values.length; i++ ) {
-            col.set(i, values[i]);
-        }
-
-        return col;
-    }
-
-    public static <T> ObjectColumn<T> newObjectColumn( String columnName, String description, T...values ) {
-        ObjectColumn<T> col = new ObjectColumnArray<>( columnName, description, values.length );
-
-        for ( int i=0; i<values.length; i++ ) {
-            col.set(i, values[i]);
-        }
-
-        return col;
-    }
-
-    @Override
-    public Iterator<Column> iterator() {
-        return Arrays.asList(columns).iterator();
-    }
 }
