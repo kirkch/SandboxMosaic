04fe5208c9f13a54bb937eb5f896fdef98c2ff59
Chris Kirk <ck@softwaremosaic.co.uk>
2014-11-11 20:11:44 +0000

    changed IntFunction1 back to an interface; as it provides better java8 support

--- lang-sandbox/src/main/java/com/mosaic/lang/functional/IntFunction1.java
+++ lang-sandbox/src/main/java/com/mosaic/lang/functional/IntFunction1.java
@@ -3,25 +3,10 @@ package com.mosaic.lang.functional;
 /**
  *
  */
-public abstract class IntFunction1<T> {
-
-    public static final IntFunction1<String> DEFAULT_INT_FORMATTER = new IntFunction1<String>() {
-        public String invoke( int v ) {
-            return Integer.toString(v);
-        }
-    };
+public interface IntFunction1<T> {
 
 
+    public T invoke( int v );
 
 
-    public abstract T invoke( int v );
-
-
-    public Function1<Integer,T> toFunction1() {
-        return new Function1<Integer, T>() {
-            public T invoke( Integer arg ) {
-                return arg == null ? null : IntFunction1.this.invoke( arg.intValue() );
-            }
-        };
-    }
 }
